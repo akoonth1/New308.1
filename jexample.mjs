f41342726e7280fa8206048a7ca406ed091426ca
akoonth1 <a.koontharana@gmail.com>
2026-01-06 18:41:25 -0500

    start of part 1

---
+++ jexample.mjs
@@ -0,0 +1,24 @@
+// jexample.mjs
+/*
+Multi line comment
+ */
+
+console.log("Hello, World!");
+
+
+let x = 5;
+let y = 10;
+
+function add(a, b) {
+    return a + b;
+}
+
+
+const n1 = 20;
+const n2 = 5;
+const n3 = 19;
+const n4 = 16;
+
+const simpleSum = n1 + n2 + n3 + n4;
+console.log("Simple Sum:", simpleSum);
+//const numberArray = [n1, n2, n3, n4];
